1fb42ea57a21fd12079854d676a47eaddb9885a9
donghoon2lee <donghoon2lee@0c873db4-d18b-6701-5bc1-337810d5f1fb>
2011-10-31 07:15:08 +0000

    Gena commit test

---
+++ src/com/lgcns/sol/upnp/gena/GenaProcessor.java
@@ -0,0 +1,20 @@
+/*================================================================================
+
+  GenaProcessor.java
+
+    하나의 SOAP 메시지를 처리하는 Processor를 나타낸다. 
+
+==================================================================================
+
+  HISTORY
+ 
+ 	# 2011년 10월 31일 : 이동훈( LDH , donghoon2lee@lgcns.com )  최초작성
+ 	
+=================================================================================*/
+package com.lgcns.sol.upnp.gena;
+
+import upnp.core.Processor;
+
+public class GenaProcessor extends Processor {
+
+}
