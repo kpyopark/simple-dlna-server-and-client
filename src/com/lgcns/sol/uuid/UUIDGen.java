e5b0c4f76914e04d87f60f737f161f6d497fa3ac
toheavener01@gmail.com <toheavener01@gmail.com@0c873db4-d18b-6701-5bc1-337810d5f1fb>
2011-11-20 08:35:19 +0000

    modified. At first, we do use only platform independent packages. java.rmi pacakge could be used only in JDK (version 1.4 above)

--- src/com/lgcns/sol/uuid/UUIDGen.java
+++ src/com/lgcns/sol/uuid/UUIDGen.java
@@ -1,14 +1,22 @@
 package com.lgcns.sol.uuid;
 
+/**
+ *  @deprecated - Use com.lgcns.sol.upnp.common.UPnPUtils.
+ * 
+ */
 public class UUIDGen {
-	  /**
-	   * Creates a new UUID. The algorithm used is the one in {@link UUID}, the
-	   * open group algorithm took too damn long.
-	   *
-	   * @return a new "globally" unique identifier
-	   */
-	  public String nextUUID() {
-	    return new java.rmi.dgc.VMID().toString();
-	  }
+	/**
+		 Our goal is to make general DLNA server & client in independent platforms.
+		 java.rmi package can be used only in JDK ( jdk version 1.4 > ), but not in android.
+		 We should make our own UUID algorithm.
+		 Please, make your code in com.lgcns.sol.common.UPnPUtils class.
+		 There is also getRandomUuid method which isn't implemented yet.
+	 * 
+	 * @return a new "globally" unique identifier
+	 */
+	public String nextUUID() {
+		return "";
+		//return new java.rmi.dgc.VMID().toString();
+	}
 
 }
