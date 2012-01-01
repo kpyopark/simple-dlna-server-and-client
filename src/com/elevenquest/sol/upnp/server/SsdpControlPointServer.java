f91b1b7cf15b550f547a710a2f8f71de53514010
toheavener01@gmail.com <toheavener01@gmail.com@0c873db4-d18b-6701-5bc1-337810d5f1fb>
2012-01-01 06:33:16 +0000

    modified. to modulize the ssdp receiver.

---
+++ src/com/elevenquest/sol/upnp/server/SsdpControlPointServer.java
@@ -0,0 +1,62 @@
+package com.elevenquest.sol.upnp.server;
+
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+
+import com.elevenquest.sol.upnp.common.UPnPUtils;
+import com.elevenquest.sol.upnp.discovery.SSDPMessage;
+import com.elevenquest.sol.upnp.exception.AbnormalException;
+import com.elevenquest.sol.upnp.model.UPnPDevice;
+import com.elevenquest.sol.upnp.network.CommonReceiveHandler;
+import com.elevenquest.sol.upnp.network.CommonReceiver;
+import com.elevenquest.sol.upnp.network.CommonSender;
+import com.elevenquest.sol.upnp.network.UDPReceiver;
+
+public class SsdpControlPointServer {
+	
+	ArrayList<CommonServer> receiveServerList = null;
+	ArrayList<CommonSender> senderServerList = null;
+
+	public void start() {
+		startReceiveServer();
+	}
+	
+	public void startReceiveServer() {
+		if ( receiveServerList != null ) 
+			stop();
+		receiveServerList = new ArrayList<CommonServer>();
+		ArrayList<NetworkInterface> interfaces = UPnPUtils.getAvailiableNetworkInterfaces();
+		for ( NetworkInterface intf : interfaces ) {
+			try {
+				// 1. Next, create ssdp message receiver & handler instance.
+				CommonReceiver receiver = new UDPReceiver(intf, UPnPDevice.DEFAULT_UPNP_MULTICAST_ADDRESS, 
+						UPnPDevice.DEFAULT_UPNP_MULTICAST_PORT);
+				CommonReceiveHandler handler = new SSDPMessage();
+				receiver.addReceiveHandler(handler);
+				// 2. Create Common server
+				CommonServer receiveServer = new CommonServer();
+				// 3. set receiver into server.
+				receiveServer.setReceiver(receiver);
+				// 4. start server.
+				receiveServer.startServer();
+				
+				receiveServerList.add(receiveServer);
+			} catch ( Exception e ) {
+				e.printStackTrace();
+			} catch ( AbnormalException abe ) {
+				abe.printStackTrace();
+			}
+		}
+	}
+	
+	public void stop() {
+		stopReceiveServer();
+	}
+	
+	public void stopReceiveServer() {
+		if ( receiveServerList != null )
+			for ( CommonServer server : receiveServerList ) server.stopServer();
+		receiveServerList = null;
+	}
+	
+}
