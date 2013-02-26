965eee274a8ef47f8f1e5809da4c5de61a04f9d0
toheavener01@gmail.com <toheavener01@gmail.com@0c873db4-d18b-6701-5bc1-337810d5f1fb>
2013-02-26 15:40:03 +0000

    Add avtransport package & one main class.

---
+++ src/com/elevenquest/sol/upnp/service/avtransport/AVTransportService.java
@@ -0,0 +1,13 @@
+package com.elevenquest.sol.upnp.service.avtransport;
+
+import com.elevenquest.sol.upnp.model.UPnPDevice;
+import com.elevenquest.sol.upnp.model.UPnPService;
+
+public class AVTransportService extends UPnPService {
+
+	public AVTransportService(UPnPDevice device) {
+		super(device);
+		// TODO Auto-generated constructor stub
+	}
+
+}
